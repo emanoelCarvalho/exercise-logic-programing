ca6ed5c9147befd1babf3131f3e9454fd94ff892
Emanoel Carvalho <113062368+emanoelCarvalho@users.noreply.github.com>
2023-05-06 20:10:08 -0300

    Update Questao-5.js

--- vetores/Nivel2/Questao-5.js
+++ vetores/Nivel2/Questao-5.js
@@ -2,15 +2,14 @@
 // vetor contendo todos os elementos entre a e b
 // Exemplo: entrada: 2 e 6, saída: [3, 4, 5]
 
-function entreVetor(array, inteiro1, inteiro2) {
-    for (let i = inteiro1; i < inteiro2; i++) {
-       array.push(i);
+
+function pushParalelo(a, b) {
+    let vazio = []
+
+    for (let i = a + 1, j = 0; i < b; i++, j++) {
+        vazio[j] = i
     }
-    console.log(array);
+    console.log(vazio)
 }
 
-let array = [];
-let inteiro1 = 6;
-let inteiro2 = 6;
-
-entreVetor(array, inteiro1, inteiro2);
+pushParalelo(1, 7)
