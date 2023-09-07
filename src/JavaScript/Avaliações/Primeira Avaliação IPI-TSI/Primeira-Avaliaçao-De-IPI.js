035309bb71b48052f73f2d1dce4dadf47677a26f
emanoelCarvalho <ehcs@discente.ifpe.edu.br>
2023-09-07 13:54:29 -0300

    :sparkles: feat: diretório de Primeira Avaliação IPI-TSI totalmente comentados

--- src/JavaScript/Avaliações/Primeira Avaliação IPI-TSI/Primeira-Avaliaçao-De-IPI.js
+++ src/JavaScript/Avaliações/Primeira Avaliação IPI-TSI/Primeira-Avaliaçao-De-IPI.js
@@ -1,28 +1,33 @@
-/*1°) Avaliação a nota da 1ª unidade de uma turma é dada pela média ponderada de dois números de acordo com a fórmula: ((p1 x a) + (p2 x b))
+/*1°) Avaliação - A nota da 1ª unidade de uma turma é dada pela média ponderada de dois números de acordo com a fórmula: ((p1 x a) + (p2 x b))
  dividido pela soma dos pesos, onde a é a nota da prova, b é a nota das atividades e p1 e p2 são os pesos. Implemente um programa capaz de 
  computar a média de um estudante na primeira unidade e indicar se o estudante foi aprovado (média maior ou igual a 6.0) ou não.*/
 
+// Definindo os pesos, notas da prova e atividades
 let p1 = 2;
 let p2 = 3;
 let a = 7;
 let b = 8;
+
+// Calculando a média ponderada
 let media = ((p1 * a) + (p2 * b)) / (p1 + p2);
 
+// Verificando se o estudante foi aprovado
 if (media >= 6) {
     console.log('1° : ' ,"Aprovado");
 } else {
     console.log('1° : ' , "Reprovado");
 }
 
-/*2°)Transforme sua resposta da questão 1 em um subprograma que recebe como parâmetros as notas do estudante e os pesos, e retorna true se ele 
+/*2°) Transforme sua resposta da questão 1 em um subprograma que recebe como parâmetros as notas do estudante e os pesos, e retorna true se ele 
 foi aprovado ou false caso contrário.*/
 
+// Função para calcular a média ponderada e verificar se o estudante foi aprovado
 function mediaPonderada(p1, p2, a, b) {
     let media = ((p1 * a) + (p2 * b)) / (p1 + p2);
     if (media >= 6) {
-        return media + ' aprovado';
+        return true;
     } else {
-        return  media +' reprovado';
+        return false;
     }
 }
 
@@ -32,9 +37,11 @@ console.log('2° : ' , mediaPonderada(2, 3, 7, 8));
 /*3°) Implemente um programa que imprime quantos divisores pares um número possui. Exemplo, se a entrada
  do programa for 12 sua saída deverá ser 4 (note que 2, 4, 6 e 12 são os divisores pares de 12).*/
 
+// Definindo o número para o qual queremos encontrar os divisores pares
 let numero = 12;
 let divisores = 0;
 
+// Usando um loop for para encontrar divisores pares
 for (let i = 1; i <= numero; i++) {
     if (numero % i == 0 && i % 2 == 0) {
         divisores++;
@@ -45,6 +52,7 @@ console.log('3° : ' ,divisores);
 
 /*4°) Transforme sua resposta da questão 3 em um subprograma que recebe como parâmetro um número e  retorna a quantidade de divisores pares dele.*/
 
+// Função para encontrar a quantidade de divisores pares de um número
 function divisoresPares(numero) {
     let divisores = 0;
     for (let i = 1; i <= numero; i++) {
@@ -55,4 +63,4 @@ function divisoresPares(numero) {
     return divisores;
 }
 
-console.log('4° : ' ,divisoresPares(12));
+console.log('4° : ' ,divisoresPares(12)); 
--- src/JavaScript/Avaliações/Primeira Avaliação IPI-TSI/Primeira-Avaliaçao-De-TSI.js
+++ src/JavaScript/Avaliações/Primeira Avaliação IPI-TSI/Primeira-Avaliaçao-De-TSI.js
@@ -3,11 +3,14 @@
   Implemente um programa capaz de computar a média harmônica de um estudante e indicar se o estudante deve fazer a 
   prova de recuperação (média < 6.0) ou não.*/
 
+// Definindo as notas da prova e atividades
 let notaProva = 7;
 let notaAtividades = 8;
 
+// Calculando a média harmônica
 let mediaHarmonica = (2 * (notaProva * notaAtividades)) / (notaProva + notaAtividades);
 
+// Verificando se o estudante deve fazer a prova de recuperação
 if (mediaHarmonica < 6) {
     console.log('1° : ',mediaHarmonica, "Estudante deve fazer prova de recuperação");
 } else {
@@ -18,12 +21,13 @@ if (mediaHarmonica < 6) {
  recebe como parâmetros as notas do estudante e retorna true se ele foi aprovado ou 
  false caso contrário.*/
 
- function media (notaProva, notaAtividades) {
+// Função para calcular a média harmônica e verificar se o estudante foi aprovado
+function media (notaProva, notaAtividades) {
     let mediaHarmonica = (2 * (notaProva * notaAtividades)) / (notaProva + notaAtividades);
     if (mediaHarmonica < 6) {
-        return mediaHarmonica + ' aprovado';
+        return false;
     } else {
-        return mediaHarmonica + ' reprovado';
+        return true;
     }
 }   
 
@@ -32,9 +36,11 @@ console.log('2° : ', media(7, 8));
 /*3°) Implemente um programa que imprime quantos divisores ímpares um número possui. Exemplo, 
 se a entrada do programa for 12 sua saída deverá ser 2 (note que 1 e 3 são os divisores ímpares de 12).*/
 
+// Definindo o número para o qual queremos encontrar os divisores ímpares
 let numero = 12;
 let divisores = 0;
 
+// Usando um loop for para encontrar divisores ímpares
 for (let i = 1; i <= numero; i++) {
     if (numero % i == 0 && i % 2 != 0) {
         divisores++;
@@ -45,6 +51,7 @@ console.log('3° : ',divisores);
 
 /*4°)Transforme sua resposta da questão 3 em um subprograma que recebe como parâmetro um número retorna a quantidade de divisores ímpares dele.*/
 
+// Função para encontrar a quantidade de divisores ímpares de um número
 function divisoresImpares(numero) {
     let divisores = 0;
     for (let i = 1; i <= numero; i++) {
@@ -56,4 +63,3 @@ function divisoresImpares(numero) {
 }
 
 console.log('4° : ', divisoresImpares(12));
-
